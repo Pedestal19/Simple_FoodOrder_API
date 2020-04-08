2bd78ee889d4f007977eca96e73ccd66a95b8776
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-08 22:05:27 +0100

    wrote spring application tests

--- src/test/java/com/foodOrder/FoodFoodOrdersApplicationTests.java
+++ src/test/java/com/foodOrder/FoodFoodOrdersApplicationTests.java
@@ -1,13 +1,29 @@
 package com.foodOrder;
 
+import com.foodOrder.controllers.FoodOrderController;
+import com.foodOrder.controllers.MealController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FoodFoodOrdersApplicationTests {
 
+	@Autowired
+	FoodOrderController foodOrderController;
+
+	@Autowired
+	MealController mealController;
+
 	@Test
 	void contextLoads() {
+		Assert.assertNotNull(foodOrderController);
+		Assert.assertNotNull(mealController);
 	}
+
 
 }
