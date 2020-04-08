8c11a11c1ca098887132f395afe8075ed83f5f38
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-08 22:16:52 +0100

    wrote system tests

--- src/test/java/com/foodOrder/SystemsTests.java
+++ src/test/java/com/foodOrder/SystemsTests.java
@@ -1,6 +1,7 @@
 package com.foodOrder;
 
 import com.foodOrder.model.FoodOrders;
+import com.foodOrder.model.Meal;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class SystemsTests {
 
     @Test
-    public void testCreateReadDelete(){
+    public void testCreateReadDeleteOrder(){
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -33,6 +34,26 @@ public class SystemsTests {
 //        Assertions.assertThat(restTemplate.getForObject(url, Friend[].class)).isEmpty();
         FoodOrders[] foodOrderAfterDelete = restTemplate.getForObject(url, FoodOrders[].class);
         Assertions.assertThat(foodOrderAfterDelete).extracting(FoodOrders::getId).doesNotContain(entity.getBody().getId());
+
+    }
+
+    @Test
+    public void testCreateReadDeleteMeal(){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:7000/meal";
+
+        Meal meal = new Meal("spaghetti", 1200);
+        ResponseEntity<Meal> entity = restTemplate.postForEntity(url, meal, Meal.class);
+
+        Meal[] meals = restTemplate.getForObject(url, Meal[].class);
+        Assertions.assertThat(meals).extracting(Meal::getName).contains("spaghetti");
+
+        restTemplate.delete(url + "/" +entity.getBody().getId());
+//        Assertions.assertThat(restTemplate.getForObject(url, Friend[].class)).isEmpty();
+        Meal[] mealAfterDelete = restTemplate.getForObject(url, Meal[].class);
+        Assertions.assertThat(mealAfterDelete).extracting(Meal::getId).doesNotContain(entity.getBody().getId());
 
     }
 
